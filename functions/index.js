fe1eba35c94c15b42af1891f0c9247404adea561
tesol2y090 <gliese090@gmail.com>
2019-07-08 00:13:36 +0700

    fix runtime

--- functions/index.js
+++ functions/index.js
@@ -9,32 +9,43 @@ const LINE_HEADER = {
   Authorization : `Bearer njqYzbipxGtAvq1saQ7jpBgE2niY+dwx5uNDPw6i9zGW4v7J7bx65Gzq1QnQYK2Hp3C5bDKvx7ZwHtj7HpwGvYmdLrE+CPfb8+A4sCZ7l9dTznsMIzBZyamADZmxnzBRo7XtSRqqkhsoAfgf2WAbrwdB04t89/1O/w1cDnyilFU=`
 };
 const region = 'asia-east2';
+const runtimeOpts = {
+  timeoutSeconds: 5,
+  memory: "2GB"
+};
 
-exports.LineBot = functions.region(region).https.onRequest((req, res) => {
+exports.LineBot = functions.region(region).runWith(runtimeOpts).https.onRequest((req, res) => {
 
   let event = req.body.events[0]
   let replyToken = event.replyToken
   let text = event.message.text
   let userId = event.source.userId
   let timeStamp = event.timestamp
-  let date = timeConverter(timeStamp, fullDate)
 
   if (event.message.type !== 'text') {
       return;
   }
-  if (event.message.text === 'fuck') {
-    replyDefault(replyToken)
-  } else {
-    writeData(replyToken, text, userId, date);
-    reply(req.body);  
-  }
+
+  console.log("Event in group")
+  console.log(req.body)
+  console.log("Source in Event")
+  console.log(event.source)
+  console.log("Event in Message")
+  console.log(event.message)
+
+  writeData(replyToken, text, userId, timeStamp);
+
 });
 
-const writeData = (replyToken, text, userId, date) => {
-  admin.database().ref('test' + replyToken).set({
+const writeData = (replyToken, text, userId, timeStamp) => {
+
+  let fullDate = timeConverter(timeStamp, 'fullDate')
+  let dateMonth = timeConverter(timeStamp, 'dateMonth')
+
+  admin.database().ref(fullDate + '/' + replyToken).set({
     'userId': userId,
     'text': text,
-    'timeStamp': date
+    'timeStamp': dateMonth
   })
 }
 
@@ -79,13 +90,13 @@ const timeConverter = (timeStamp, option) => {
   let hour = a.getHours() + 7;
   let min = a.getMinutes();
   let sec = a.getSeconds();
-  let date = date + " " + month + " " + year
+  let fullDate = date + ' ' + month + ' ' + year
   let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
 
   if(option === 'dateMonth') {
     return time
   } else if(option === 'fullDate'){
-    return date
+    return fullDate
   } else if(option === 'day') {
     return date
   } else if(option === 'month') {
