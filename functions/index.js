6884bca7b22216a6baa71b043bb2fc93a1cbf103
tesol2y090 <gliese090@gmail.com>
2019-07-07 20:31:00 +0700

    add time convert

--- functions/index.js
+++ functions/index.js
@@ -6,36 +6,35 @@ admin.initializeApp();
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
 const LINE_HEADER = {
   'Content-Type': 'application/json',
-  Authorization : `Bearer access token`
+  Authorization : `Bearer njqYzbipxGtAvq1saQ7jpBgE2niY+dwx5uNDPw6i9zGW4v7J7bx65Gzq1QnQYK2Hp3C5bDKvx7ZwHtj7HpwGvYmdLrE+CPfb8+A4sCZ7l9dTznsMIzBZyamADZmxnzBRo7XtSRqqkhsoAfgf2WAbrwdB04t89/1O/w1cDnyilFU=`
 };
 const region = 'asia-east2';
-const runtimeOpts = {
-  timeoutSeconds: 4,
-  memory: "2GB"
-};
 
-exports.LineBot = functions.region(region).runWith(runtimeOpts).https.onRequest((req, res) => {
+exports.LineBot = functions.region(region).https.onRequest((req, res) => {
+
   let event = req.body.events[0]
   let replyToken = event.replyToken
   let text = event.message.text
   let userId = event.source.userId
   let timeStamp = event.timestamp
+  let date = timeConverter(timeStamp, fullDate)
+
   if (event.message.type !== 'text') {
       return;
   }
   if (event.message.text === 'fuck') {
     replyDefault(replyToken)
   } else {
-    writeData(replyToken, text, userId, timeStamp);
+    writeData(replyToken, text, userId, date);
     reply(req.body);  
   }
 });
 
-const writeData = (replyToken, text, userId, timeStamp) => {
+const writeData = (replyToken, text, userId, date) => {
   admin.database().ref('test' + replyToken).set({
     'userId': userId,
     'text': text,
-    'timeStamp': timeStamp
+    'timeStamp': date
   })
 }
 
@@ -70,3 +69,30 @@ const reply = (bodyResponse) => {
     })
   });
 };
+
+const timeConverter = (timeStamp, option) => {
+  let a = new Date(timeStamp);
+  let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  let year = a.getFullYear();
+  let month = months[a.getMonth()];
+  let date = a.getDate();
+  let hour = a.getHours() + 7;
+  let min = a.getMinutes();
+  let sec = a.getSeconds();
+  let date = date + " " + month + " " + year
+  let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+
+  if(option === 'dateMonth') {
+    return time
+  } else if(option === 'fullDate'){
+    return date
+  } else if(option === 'day') {
+    return date
+  } else if(option === 'month') {
+    return month
+  } else if(option === 'year') {
+    return year
+  } else {
+    return 'not define'
+  }
+}
