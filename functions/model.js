3e21e616d2869fab8efc1b6585cce90d21bd709e
tesol2y090 <gliese090@gmail.com>
2019-07-14 22:49:27 +0700

    add word preprocessing

--- functions/model.js
+++ functions/model.js
@@ -1,9 +1,42 @@
 const wordcut = require('wordcut')
 wordcut.init();
 
-let message = "สรุป วันนี้มีประชุมตอน 6 โมงนะครับ"
-let textCut = wordcut.cut(message)
-let textSplit = textCut.split("|").filter(word => word !== " ")
+const MAX_SEQUENCE_LENGTH = 113;
 
+const wordPrep = word => {
+    word = word.replace(/[-|.|,|\?|\!]+/g, '');
+    word = word.toLowerCase();
+    if (word != '') {
+      return word;
+    } else {
+      return '.'
+    }
+}
 
-console.log(textSplit)
+function makeSeq(wordArray) {
+    let sequence = [];
+    
+    wordArray.slice(0, MAX_SEQUENCE_LENGTH).forEach(function(word) {
+    word = wordPrep(word);
+    sequence.push(word);
+  });
+
+  // pad sequence
+  if (sequence.length < MAX_SEQUENCE_LENGTH) {
+    let pad_array = Array(MAX_SEQUENCE_LENGTH - sequence.length);
+    pad_array.fill("");
+    sequence = sequence.concat(pad_array);
+  }
+
+  return sequence;
+}
+
+let message = "📣📣 VIDWEEK IS COMINGและตอนนี้เทศกาลรับน้องใหม่ คณะวิทยาศาสตร์หรือว่า VIDWEEK 2019 ของเราก็ใกล้เข้ามาทุกที่แล้วนะครับ ก่อนอื่นเลย เราต้องมาประกาศธีมก่อนก่อนเลยว่า VIDWEEK ของเราในปีนี้นั้น มาในธีม!🌌 “VIDYA AGRABAH”🌌 ‼️ซึ่ง อ่านว่า วิทยา อัคราบานะครับ อัคราบาก็คือเมืองของเจ้าหญิงจัสมินกับเจ้าชายอลาดินในเรื่องอลาดินนั่นเอง"
+let wordArray = wordcut.cut(message).split("|").filter(word => word !== " ")
+if (wordArray.length < MAX_SEQUENCE_LENGTH) {
+    let pad_array = Array(MAX_SEQUENCE_LENGTH - wordArray.length);
+    pad_array.fill("");
+    wordArray = wordArray.concat(pad_array);
+  }
+
+console.log(wordArray)
